63cf1aff5fd0c4881e94949877dda26907b7a0ff
Lawrence Mandel <lmandel@gmail.com>
2012-07-28 00:45:20 -0400

    Added find/fix rate. Refactored dateString code.

--- js/burndown.js
+++ js/burndown.js
@@ -10,16 +10,7 @@ var productIssueDate = {};
 function createBurndownChart(){
 	var date = new Date(startDate);
 	while (date.getTime() < today.getTime()){
-		var year = date.getFullYear();
-		var month = date.getMonth()+1;
-		if (month < 10){
-			month = "0" + month;
-		}
-		var day = date.getDate();
-		if(day < 10){
-			day = "0" + day;
-		}
-		var dateString = year + "-" + month + "-" + day;
+		var dateString = getDateString(date);
 		getDataForDate(dateString);
 		date.setDate(date.getDate()+1);
 	}
@@ -35,16 +26,7 @@ function drawBurndownChart() {
 		endDate.setDate(endDate.getDate()+2);
 		var i = 1;
 		while(date.getTime() < endDate.getTime()){
-			var year = date.getFullYear();
-			var month = date.getMonth()+1;
-			if (month < 10){
-				month = "0" + month;
-			}
-			var day = date.getDate();
-			if(day < 10){
-				day = "0" + day;
-			}
-			var dateString = year + "-" + month + "-" + day;
+			var dateString = getDateString(date);
 			
 			if(issueDate[dateString] === undefined){
 				data[i] = [dateString, null, null, null];
@@ -70,6 +52,7 @@ function drawBurndownChart() {
         chart.draw(chartData, options);
         
         drawProductBurndownCharts();
+        calculateFindFixRates();
 	}
 }
 
@@ -88,16 +71,7 @@ function drawProductBurndownCharts(){
 		endDate.setDate(endDate.getDate()+2);
 		var i = 1;
 		while(date.getTime() < endDate.getTime()){
-			var year = date.getFullYear();
-			var month = date.getMonth()+1;
-			if (month < 10){
-				month = "0" + month;
-			}
-			var day = date.getDate();
-			if(day < 10){
-				day = "0" + day;
-			}
-			var dateString = year + "-" + month + "-" + day;
+			var dateString = getDateString(date);
 			
 			if(productIssueDate[product][dateString] === undefined){
 				data[i] = [dateString, null, null, null];
@@ -132,16 +106,7 @@ function drawProductBurndownCharts(){
 function dataIsInForAllDates(){
 	var date = new Date(startDate);
 	while(date.getTime() < today.getTime()){
-		var year = date.getFullYear();
-		var month = date.getMonth()+1;
-		if (month < 10){
-			month = "0" + month;
-		}
-		var day = date.getDate();
-		if(day < 10){
-			day = "0" + day;
-		}
-		var dateString = year + "-" + month + "-" + day;
+		var dateString = getDateString(date);
 		if(issueDate[dateString] === undefined){
 			return false;
 		}
@@ -149,8 +114,6 @@ function dataIsInForAllDates(){
 	}
 	return true;
 }
-
-
 
 function getDataForDate(date){
 	var urlbase = "data/";
@@ -236,4 +199,51 @@ function parseDataForDate(date, data){
 function setEmptyDataForDate(date){
 	issueDate[date] = [date,null,null,null];
 	drawBurndownChart();
+}
+
+function calculateFindFixRates(){
+	var rateDiv = document.getElementById("rate_div");
+	var endDate = new Date(Date.now());
+	var endDateString = getDateString(endDate);
+
+	for (var product in products) {
+		while(productIssueDate[product][endDateString] === undefined){
+			endDate.setDate(endDate.getDate()-1);
+			endDateString = getDateString(endDate);
+		}
+	    break;
+	}
+	var startDate = new Date();
+	startDate.setDate(endDate.getDate()-7);
+	var startDateString = getDateString(startDate);
+
+	for(var product in products){
+		var endDateTotal = productIssueDate[product][endDateString][1];
+		var endDateOpen = productIssueDate[product][endDateString][2];
+		var startDateTotal = productIssueDate[product][startDateString][1];
+		var startDateOpen = productIssueDate[product][startDateString][2];
+
+		var find = endDateTotal - startDateTotal;
+		var fix = (endDateTotal - endDateOpen) - (startDateTotal - startDateOpen);
+		
+		var rate = Math.round((find/fix)*100)/100;
+		
+		var div = document.createElement('div');
+		div.setAttribute("class", "findfixrate");
+		div.innerHTML = products[product] + ": " +rate;
+		rateDiv.appendChild(div);
+	}
+}
+
+function getDateString(date){
+	var year = date.getFullYear();
+	var month = date.getMonth()+1;
+	if (month < 10){
+		month = "0" + month;
+	}
+	var day = date.getDate();
+	if(day < 10){
+		day = "0" + day;
+	}
+	return year + "-" + month + "-" + day;
 }
