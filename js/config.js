05460c2957ec51f796ec3aaaba41b1c004c6f33f
Lawrence Mandel <lmandel@gmail.com>
2012-08-31 10:55:04 -0400

    Changed end date to Sept 30.

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 var startDate = "2012-07-15";
 var today = new Date(Date.now());
 var actualStartDate = "2012-07-14";
-var actualEndDate = "2012-08-31";
+var actualEndDate = "2012-09-30";
 var showTrack = false;
