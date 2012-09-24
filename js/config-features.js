c000823d6d0db3c65d2df6a9f10a15d1f39c0529
Lawrence Mandel <lmandel@gmail.com>
2012-09-24 11:07:13 -0400

    Enabled track line for features dashboard.

--- js/config-features.js
+++ js/config-features.js
@@ -2,5 +2,5 @@ var startDate = "2012-09-23";
 var today = new Date(Date.now());
 var actualStartDate = "2012-09-22";
 var actualEndDate = "2012-09-28";
-var showTrack = false;
+var showTrack = true;
 var featuresOnly = true;
