0b07f1f763cf1c68a4d4b45b8ed2d5f23546bbbc
Lawrence Mandel <lmandel@gmail.com>
2012-09-29 20:28:57 -0400

    Turned tracking line off for features.

--- js/config-features.js
+++ js/config-features.js
@@ -2,5 +2,5 @@ var startDate = "2012-09-23";
 var today = new Date(Date.now());
 var actualStartDate = "2012-09-22";
 var actualEndDate = "2012-09-28";
-var showTrack = true;
+var showTrack = false;
 var featuresOnly = true;
