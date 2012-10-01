e9526057f7af5f3b9569c312901720556f48005b
Lawrence Mandel <lmandel@gmail.com>
2012-10-01 17:04:36 -0400

    Added issues page for complete issue tracking from July 2012. Updated main burndown to start Oct. 1. Added nav links for three pages.

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
-var startDate = "2012-07-15";
+var startDate = "2012-10-02";
 var today = new Date(Date.now());
-var actualStartDate = "2012-07-14";
-var actualEndDate = "2012-11-30";
-var showTrack = false;
+var actualStartDate = "2012-10-01";
+var actualEndDate = "2013-01-15";
+var showTrack = true;
 var featuresOnly = false;
