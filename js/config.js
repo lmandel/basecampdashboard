cde8a53bc3c50087145185dc9a5d1ca9d57b00f9
Lawrence Mandel <lmandel@gmail.com>
2012-10-03 10:15:34 -0400

    Changed start date to follow mass migration of Gaia issues to Bugzilla.

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
-var startDate = "2012-10-02";
+var startDate = "2012-10-03";
 var today = new Date(Date.now());
-var actualStartDate = "2012-10-01";
+var actualStartDate = "2012-10-02";
 var actualEndDate = "2013-01-15";
 var showTrack = true;
 var featuresOnly = false;
